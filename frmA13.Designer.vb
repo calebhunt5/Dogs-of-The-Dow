666fa972c8af475c799ac3f1575d901632b64b31
Caleb Hunter <64340885+calebhunt5@users.noreply.github.com>
2021-01-23 22:44:10 -0500

    Add files via upload

---
+++ frmA13.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmA13
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grpDogs = New System.Windows.Forms.GroupBox()
+        Me.lstDogs = New System.Windows.Forms.ListBox()
+        Me.grpDogs.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpDogs
+        '
+        Me.grpDogs.Controls.Add(Me.lstDogs)
+        Me.grpDogs.Font = New System.Drawing.Font("Ebrima", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDogs.Location = New System.Drawing.Point(12, 12)
+        Me.grpDogs.Name = "grpDogs"
+        Me.grpDogs.Size = New System.Drawing.Size(1034, 598)
+        Me.grpDogs.TabIndex = 0
+        Me.grpDogs.TabStop = False
+        Me.grpDogs.Text = "Dogs of the Dow"
+        '
+        'lstDogs
+        '
+        Me.lstDogs.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDogs.FormattingEnabled = True
+        Me.lstDogs.ItemHeight = 23
+        Me.lstDogs.Location = New System.Drawing.Point(6, 30)
+        Me.lstDogs.Name = "lstDogs"
+        Me.lstDogs.Size = New System.Drawing.Size(1018, 556)
+        Me.lstDogs.TabIndex = 0
+        '
+        'frmA13
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1056, 622)
+        Me.Controls.Add(Me.grpDogs)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmA13"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Assignment 13 - Dogs of the Dow"
+        Me.grpDogs.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents grpDogs As GroupBox
+    Friend WithEvents lstDogs As ListBox
+End Class
---
+++ frmA13.vb
@@ -0,0 +1,53 @@
+﻿Public Class frmA13
+    'creation of constants for indexes in csv
+    Const IDX_SYMBOL As Integer = 0
+    Const IDX_NAME As Integer = 1
+    Const IDX_2018 As Integer = 2
+    Const IDX_2019 As Integer = 3
+
+    'array that holds stores csv values
+    Dim dogs() As String
+
+    'creation of formats for header and company data
+    Dim headerFormat As String = "{0, -20}|{1, 10}|{2, 15}|{3, 15}|{4, 15}"
+    Dim dogsFormat As String = "{0, -20}|{1, 10}|{2, 15:N2}|{3, 15:N2}|{4, 15:p}"
+
+    'form load is the only method in this program
+    Private Sub frmA13_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'populates dogs with csv file data
+        dogs = IO.File.ReadAllLines("DOW2019.txt")
+
+        'output horizontal line
+        lstDogs.Items.Add(StrDup(80, "-"))
+
+        'query for company info
+        Dim dogsQuery = From company In dogs
+                        Let symbol = company.Split(","c)(IDX_SYMBOL)
+                        Let name As String = company.Split(","c)(IDX_NAME)
+                        Let prevYear = CDec(company.Split(","c)(IDX_2018))
+                        Let nextYear = CDec(company.Split(","c)(IDX_2019))
+                        Let priceDiff As Decimal = nextYear - prevYear
+                        Let percentChange As Decimal = priceDiff / prevYear
+                        Select symbol, name, prevYear, nextYear, percentChange
+
+        'output header and another horizontal line
+        lstDogs.Items.Add(String.Format(headerFormat,
+                                        "Company",
+                                        "Symbol",
+                                        "Price 2018",
+                                        "Price 2019",
+                                        "Change %"))
+        lstDogs.Items.Add(StrDup(80, "-"))
+
+        'loop to output each company's info and a horizontal line
+        For Each company In dogsQuery
+            lstDogs.Items.Add(String.Format(dogsFormat,
+                                            company.name,
+                                            company.symbol,
+                                            company.prevYear,
+                                            company.nextYear,
+                                            company.percentChange))
+            lstDogs.Items.Add(String.Format(StrDup(80, "-")))
+        Next
+    End Sub
+End Class
